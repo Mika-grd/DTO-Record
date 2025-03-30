b581e315843ea2ea454562a30f0753074f7f004d
Wjuanpablo26 <juanp.reyg@uqvirtual.edu.co>
2025-03-29 22:58:54 -0500

    commit controllers

--- src/main/java/co/edu/uniquindio/poo/dtorecord/controller/EstadisticasEquipoController.java
+++ src/main/java/co/edu/uniquindio/poo/dtorecord/controller/EstadisticasEquipoController.java
@@ -1,78 +1,111 @@
 package co.edu.uniquindio.poo.dtorecord.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import co.edu.uniquindio.poo.dtorecord.model.DesempeñoEquipo;
+import co.edu.uniquindio.poo.dtorecord.model.Equipo;
+import co.edu.uniquindio.poo.dtorecord.model.Grupo;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-
-import java.net.URL;
-import java.util.ResourceBundle;
-
-
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 public class EstadisticasEquipoController {
 
+    @FXML
+    private ResourceBundle resources;
 
+    @FXML
+    private URL location;
 
+    @FXML
+    private Button btnActualizar;
 
-        @FXML
-        private ResourceBundle resources;
+    @FXML
+    private Button btnRegresar;
 
-        @FXML
-        private URL location;
+    @FXML
+    private TableColumn<DesempeñoEquipo, Integer> colGolesAFavor;
 
-        @FXML
-        private Button btnRegresar;
+    @FXML
+    private TableColumn<DesempeñoEquipo, Integer> colGolesEnContra;
 
-        @FXML
-        private TableColumn<?, ?> colGolesAFavor;
+    @FXML
+    private TableColumn<DesempeñoEquipo, Integer> colPartidosEmpatados;
 
-        @FXML
-        private TableColumn<?, ?> colGolesEnContra;
+    @FXML
+    private TableColumn<DesempeñoEquipo, Integer> colPartidosGanados;
 
-        @FXML
-        private TableColumn<?, ?> colPartidosEmpatados;
+    @FXML
+    private TableColumn<DesempeñoEquipo, Integer> colPartidosJugados;
 
-        @FXML
-        private TableColumn<?, ?> colPartidosGanados;
+    @FXML
+    private TableColumn<DesempeñoEquipo, Integer> colPartidosPerdidos;
 
-        @FXML
-        private TableColumn<?, ?> colPartidosJugados;
+    @FXML
+    private TableColumn<DesempeñoEquipo, Integer> colPuntos;
 
-        @FXML
-        private TableColumn<?, ?> colPartidosPerdidos;
+    @FXML
+    private ComboBox<Equipo> comboEquipos;
 
-        @FXML
-        private TableColumn<?, ?> colPuntos;
+    @FXML
+    private TableView<DesempeñoEquipo> tablaEstadisticas;
 
-        @FXML
-        private ComboBox<?> comboEquipos;
+    private ObservableList<Equipo> listaEquipos;
+    private ObservableList<DesempeñoEquipo> listaEstadisticas;
 
-        @FXML
-        private TableView<?> tablaEstadisticas;
-
-        @FXML
-        void volverAlMenu(ActionEvent event) {
-
+    @FXML
+    void actualizarTabla(ActionEvent event) {
+        Equipo equipoSeleccionado = comboEquipos.getValue();
+        if (equipoSeleccionado != null) {
+            listaEstadisticas.setAll(new DesempeñoEquipo(equipoSeleccionado));
         }
-
-        @FXML
-        void initialize() {
-            assert btnRegresar != null : "fx:id=\"btnRegresar\" was not injected: check your FXML file 'EstadisticasEquipo.fxml'.";
-            assert colGolesAFavor != null : "fx:id=\"colGolesAFavor\" was not injected: check your FXML file 'EstadisticasEquipo.fxml'.";
-            assert colGolesEnContra != null : "fx:id=\"colGolesEnContra\" was not injected: check your FXML file 'EstadisticasEquipo.fxml'.";
-            assert colPartidosEmpatados != null : "fx:id=\"colPartidosEmpatados\" was not injected: check your FXML file 'EstadisticasEquipo.fxml'.";
-            assert colPartidosGanados != null : "fx:id=\"colPartidosGanados\" was not injected: check your FXML file 'EstadisticasEquipo.fxml'.";
-            assert colPartidosJugados != null : "fx:id=\"colPartidosJugados\" was not injected: check your FXML file 'EstadisticasEquipo.fxml'.";
-            assert colPartidosPerdidos != null : "fx:id=\"colPartidosPerdidos\" was not injected: check your FXML file 'EstadisticasEquipo.fxml'.";
-            assert colPuntos != null : "fx:id=\"colPuntos\" was not injected: check your FXML file 'EstadisticasEquipo.fxml'.";
-            assert comboEquipos != null : "fx:id=\"comboEquipos\" was not injected: check your FXML file 'EstadisticasEquipo.fxml'.";
-            assert tablaEstadisticas != null : "fx:id=\"tablaEstadisticas\" was not injected: check your FXML file 'EstadisticasEquipo.fxml'.";
-
-        }
-
     }
 
+    @FXML
+    void volverAlMenu(ActionEvent event) {
+        Stage stageActual = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stageActual.close();
+    }
+
+    @FXML
+    void initialize() {
+        assert btnActualizar != null : "fx:id=\"btnActualizar\" was not injected: check your FXML file 'EstadisticasEquipo.fxml'.";
+        assert btnRegresar != null : "fx:id=\"btnRegresar\" was not injected: check your FXML file 'EstadisticasEquipo.fxml'.";
+        assert colGolesAFavor != null : "fx:id=\"colGolesAFavor\" was not injected: check your FXML file 'EstadisticasEquipo.fxml'.";
+        assert colGolesEnContra != null : "fx:id=\"colGolesEnContra\" was not injected: check your FXML file 'EstadisticasEquipo.fxml'.";
+        assert colPartidosEmpatados != null : "fx:id=\"colPartidosEmpatados\" was not injected: check your FXML file 'EstadisticasEquipo.fxml'.";
+        assert colPartidosGanados != null : "fx:id=\"colPartidosGanados\" was not injected: check your FXML file 'EstadisticasEquipo.fxml'.";
+        assert colPartidosJugados != null : "fx:id=\"colPartidosJugados\" was not injected: check your FXML file 'EstadisticasEquipo.fxml'.";
+        assert colPartidosPerdidos != null : "fx:id=\"colPartidosPerdidos\" was not injected: check your FXML file 'EstadisticasEquipo.fxml'.";
+        assert colPuntos != null : "fx:id=\"colPuntos\" was not injected: check your FXML file 'EstadisticasEquipo.fxml'.";
+        assert comboEquipos != null : "fx:id=\"comboEquipos\" was not injected: check your FXML file 'EstadisticasEquipo.fxml'.";
+        assert tablaEstadisticas != null : "fx:id=\"tablaEstadisticas\" was not injected: check your FXML file 'EstadisticasEquipo.fxml'.";
+
+        listaEquipos = FXCollections.observableArrayList(Grupo.getInsance().getListaEquipos());
+
+        comboEquipos.setItems(listaEquipos);
+
+        colPartidosJugados.setCellValueFactory(new PropertyValueFactory<>("partidosJugados"));
+        colPartidosGanados.setCellValueFactory(new PropertyValueFactory<>("partidosGanados"));
+        colPartidosEmpatados.setCellValueFactory(new PropertyValueFactory<>("partidosEmpatados"));
+        colPartidosPerdidos.setCellValueFactory(new PropertyValueFactory<>("partidosPerdidos"));
+        colGolesAFavor.setCellValueFactory(new PropertyValueFactory<>("golesAFavor"));
+        colGolesEnContra.setCellValueFactory(new PropertyValueFactory<>("golesEnContra"));
+        colPuntos.setCellValueFactory(new PropertyValueFactory<>("puntos"));
+
+        listaEstadisticas = FXCollections.observableArrayList();
+        tablaEstadisticas.setItems(listaEstadisticas);
+    }
+
+    
+}
 
--- src/main/java/co/edu/uniquindio/poo/dtorecord/controller/TablaClasificacionController.java
+++ src/main/java/co/edu/uniquindio/poo/dtorecord/controller/TablaClasificacionController.java
@@ -1,49 +1,81 @@
 package co.edu.uniquindio.poo.dtorecord.controller;
 
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import co.edu.uniquindio.poo.dtorecord.HelloApplication;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-
-import javax.swing.table.TableColumn;
-import java.net.URL;
-import java.util.ResourceBundle;
+import javafx.scene.control.cell.PropertyValueFactory;
+import co.edu.uniquindio.poo.dtorecord.model.Equipo;
+import co.edu.uniquindio.poo.dtorecord.model.TablaClasificacion;
+import co.edu.uniquindio.poo.dtorecord.model.DesempeñoEquipo;
+import javafx.stage.Stage;
 
 public class TablaClasificacionController {
 
-    
-        @FXML
-        private ResourceBundle resources;
+    @FXML
+    private ResourceBundle resources;
 
-        @FXML
-        private URL location;
+    @FXML
+    private URL location;
 
-        @FXML
-        private Button btnRegresar;
+    @FXML
+    private Button btnActualizar;
 
-        @FXML
-        private TableColumn<?, ?> colNombreEquipo;
+    @FXML
+    private Button btnRegresar;
 
-        @FXML
-        private TableColumn<?, ?> colPuntos;
+    @FXML
+    private TableColumn<DesempeñoEquipo, String> colNombreEquipo;
 
-        @FXML
-        private TableView<?> tablaClasificacion;
+    @FXML
+    private TableColumn<DesempeñoEquipo, Integer> colPuntos;
 
-        @FXML
-        void volverAlMenu(ActionEvent event) {
+    @FXML
+    private TableView<DesempeñoEquipo> tablaClasificacion;
 
+    private ObservableList<DesempeñoEquipo> listaEquipos;
+
+    @FXML
+    void actualizarTabla(ActionEvent event) {
+        // Obtener la lista de equipos
+        List<Equipo> listaEquipos = Main.obtenerEquipos(); // Ajusta según cómo obtienes los equipos
+
+        if (listaEquipos != null && !listaEquipos.isEmpty()) {
+            // Generar la tabla de clasificación
+            TablaClasificacion tabla = TablaClasificacion.fromEquipos(listaEquipos);
+
+            // Refrescar la tabla en la UI
+            tablaClasificacion.setItems(FXCollections.observableArrayList(tabla.getEquipos()));
         }
-
-        @FXML
-        void initialize() {
-            assert btnRegresar != null : "fx:id=\"btnRegresar\" was not injected: check your FXML file 'TablaClasificacion.fxml'.";
-            assert colNombreEquipo != null : "fx:id=\"colNombreEquipo\" was not injected: check your FXML file 'TablaClasificacion.fxml'.";
-            assert colPuntos != null : "fx:id=\"colPuntos\" was not injected: check your FXML file 'TablaClasificacion.fxml'.";
-            assert tablaClasificacion != null : "fx:id=\"tablaClasificacion\" was not injected: check your FXML file 'TablaClasificacion.fxml'.";
-
-        }
-
     }
 
+    @FXML
+    void volverAlMenu(ActionEvent event) {
+        Stage stageActual = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stageActual.close();
+    }
 
+    @FXML
+    void initialize() {
+        assert btnActualizar != null : "fx:id=\"btnActualizar\" was not injected: check your FXML file 'TablaClasificacion.fxml'.";
+        assert btnRegresar != null : "fx:id=\"btnRegresar\" was not injected: check your FXML file 'TablaClasificacion.fxml'.";
+        assert colNombreEquipo != null : "fx:id=\"colNombreEquipo\" was not injected: check your FXML file 'TablaClasificacion.fxml'.";
+        assert colPuntos != null : "fx:id=\"colPuntos\" was not injected: check your FXML file 'TablaClasificacion.fxml'.";
+        assert tablaClasificacion != null : "fx:id=\"tablaClasificacion\" was not injected: check your FXML file 'TablaClasificacion.fxml'.";
+
+        colNombreEquipo.setCellValueFactory(new PropertyValueFactory<>("nombreEquipo"));
+        colPuntos.setCellValueFactory(new PropertyValueFactory<>("puntos"));
+
+        listaEquipos = FXCollections.observableArrayList();
+        tablaClasificacion.setItems(listaEquipos);
+    }
+}
