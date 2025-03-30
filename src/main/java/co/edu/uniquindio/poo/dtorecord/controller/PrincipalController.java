cec620cdb5bfa7b9012e2931c745500161f054ce
Juan. Maldonado <juanmaldo0618@gmail.com>
2025-03-29 21:42:46 -0500

    pantalla principal
    pantalla principal

--- src/main/java/co/edu/uniquindio/poo/dtorecord/controller/PrincipalController.java
+++ src/main/java/co/edu/uniquindio/poo/dtorecord/controller/PrincipalController.java
@@ -1,4 +1,49 @@
 package co.edu.uniquindio.poo.dtorecord.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class PrincipalController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button estadisticasEquiposBoton;
+
+    @FXML
+    private Button estadisticasJugadoresBoton;
+
+    @FXML
+    private Button tablaPosicionesBoton;
+
+    @FXML
+    void estadisticasEquiposAccion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void estadisticasJugadoresAccion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tablaPosicionesAccion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert estadisticasEquiposBoton != null : "fx:id=\"estadisticasEquiposBoton\" was not injected: check your FXML file 'Principal.fxml'.";
+        assert estadisticasJugadoresBoton != null : "fx:id=\"estadisticasJugadoresBoton\" was not injected: check your FXML file 'Principal.fxml'.";
+        assert tablaPosicionesBoton != null : "fx:id=\"tablaPosicionesBoton\" was not injected: check your FXML file 'Principal.fxml'.";
+
+    }
+
 }
